3ec31c04e937290921cc6483d43f628ffd551905
bowen00000000 <bowen00000000@protonmail.com>
2020-01-09 21:12:56 -0600

    adding parser

---
+++ src/brainf_parser.ml
@@ -0,0 +1,37 @@
+
+type instruction = IncDataPtr
+                 | DecDataPtr
+                 | IncData
+                 | DecData
+                 | Input
+                 | Output
+                 | JumpZero
+                 | JumpNonZero
+
+let explode_string (s: string) : char list =
+  let rec es i cl =
+    if i < 0 then
+      cl
+    else
+      es (i - 1) (s.[i]::cl)
+  in 
+    es ((String.length s) - 1) []
+
+let strip_ignored_characters (program_string: string) : string list = 
+  program_string
+  |> Str.split (Str.regexp "")
+  |> List.filter ((<>) "")
+
+let string_list_to_char_list (string_list: string list) : char list =
+  string_list
+  |> List.map explode_string
+  |> List.flatten
+
+
+let program_chars_to_tokens (chars: char list) : instruction array = [||]
+
+let parse_program_string (program_string: string) : instruction array =
+  program_string
+  |> strip_ignored_characters 
+  |> string_list_to_char_list
+  |> program_chars_to_tokens
---
+++ src/brainf_parser.mli
@@ -0,0 +1,4 @@
+
+type instruction
+
+val parse_program_string: string -> instruction array
